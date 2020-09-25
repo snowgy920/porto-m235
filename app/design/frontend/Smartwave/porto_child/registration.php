3f0b1217a6de63ab1619bad1dd945f26eabc8ed0
Jay Liu <snowgy920@hotmail.com>
2020-09-25 16:27:38 +0800

    add child theme

---
+++ app/design/frontend/Smartwave/porto_child/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2020 Porto. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Smartwave/202009_hmp_child',
+    __DIR__
+);
